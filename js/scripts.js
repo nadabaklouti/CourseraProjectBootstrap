279b6708ba7ec4cd9dbf556141f6de8b3982abd4
nada <nada.baklouti@esprit.tn>
2020-08-04 23:36:53 +0100

    NPM Scripts Part 2

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    $("#mycarousel").carousel({interval: 2000});
+
+    $("#carouselButton").click(function () {
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        } else if ($("#carouselButton").children("span").hasClass('fa-play')) {
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');
+        }
+    });
+});
+$("#myInput").click(function () {
+    $('#myModal').modal('toggle')
+})
+$("#closeBtn").click(function () {
+    $('#myModal').modal('hide')
+})
+$("#closeBtn2").click(function () {
+    $('#myModal').modal('hide')
+})
+
+$("#loginBtn").click(function () {
+    $('#loginModal').modal('toggle')
+})
+$("#loginCloseBtn").click(function () {
+    $('#loginModal').modal('hide')
+})
+$("#loginCancelBtn").click(function () {
+    $('#loginModal').modal('hide')
+})
+
